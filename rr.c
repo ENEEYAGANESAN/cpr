06a2e3ae87ade45d2d0342d696c3335082b202fd
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-01-21 10:21:17 +0530

    Update rr.c

--- rr.c
+++ rr.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int year;
-printf("enter the year");
-scanf("%d",&year);
-if(year%4==0)
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%4==0)
 {
 printf("leap year");
 }
 else
 {
-printf("not leap year");
+  printf("not leap year");
 }
-return 0;
+  return 0;
 }
