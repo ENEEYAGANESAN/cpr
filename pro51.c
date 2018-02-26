18db803a89af04d498f0388342dffb7a2292b007
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-02-26 16:54:25 +0530

    Update pro51.c

--- pro51.c
+++ pro51.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int digit,num,reverse=0;
+    int d,n,rev=0;
     printf("enter the number");
-    scanf("%d",&digit);
-while(digit!=0)
+    scanf("%d",&d);
+while(d!=0)
 {
-int number=digit%10;
-digit=digit/10;
-reverse=reverse*10+number;
+int n=d%10;
+d=d/10;
+rev=rev*10+n;
 }
-while(reverse!=0)
+while(rev!=0)
 {
-    int num=reverse%10;
-    printf("%d\t",num);
-    reverse=reverse/10;
+    int n=rev%10;
+    printf("%d\t",n);
+    rev=rev/10;
 }
     return 0;
 }
