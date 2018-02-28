0eea4a4a992c941eebf3f3409449f71502a6d3a3
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-02-28 16:23:14 +0530

    Create pro54.c

---
+++ pro54.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+int n;
+  printf("enter the n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("%d",n+2);
+    }
+    else
+    {
+        printf("%d",n-1);
+    }
+return 0;
+} 
+   
