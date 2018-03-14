6a1efe98379d6f58ceccd3602a4bc7d926a164d9
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-03-14 16:42:24 +0530

    Create pro64.c

---
+++ pro64.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//#include <string.h>
+ 
+void main()
+{
+    int n,m,a;
+    printf("enter the number");
+    scanf("%d%d",&n,&m);
+    a=n+m;
+    if(a%2==0)
+    {
+        printf("\n even number");
+    }
+        else
+        {
+        printf("\nodd number");
+}
+}
